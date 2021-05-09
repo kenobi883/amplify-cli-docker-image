e13efe6e6adb65ce270069e1e3f467ed6ceacfc4
Marc Denning <marc.denning@gmail.com>
2021-05-09 13:05:10 -0400

    Switch to full Debian Buster distro to support AWS CLI. Add AWS CLI to image.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM node:14-buster-slim
+FROM node:14-buster
+
+RUN apt-get update \
+    && apt-get install --yes groff less
+
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.2.3.zip" -o "awscliv2.zip" \
+    && unzip awscliv2.zip \
+    && ./aws/install
 
 RUN npm install -g @aws-amplify/cli
 RUN mkdir /app
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-amplify --version
+aws --version
 
-# echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"'$AMPLIFY_ENV'"}' > ./amplify/.config/local-env-info.json
+aws configure set --profile default aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set --profile default aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set --profile default region $AWS_REGION
+aws configure set --profile default output text
+
+amplify --version
 
 amplify $@
